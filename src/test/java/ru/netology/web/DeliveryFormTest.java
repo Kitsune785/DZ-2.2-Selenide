e38a102d776bd47760f2e4689da6044aac09c1bb
Anna <akhrolenko85@gmail.com>
2022-08-14 23:58:56 +0300

    изменение тестов по прямому вводу данных

--- src/test/java/ru/netology/web/DeliveryFormTest.java
+++ src/test/java/ru/netology/web/DeliveryFormTest.java
@@ -1,13 +1,17 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
 import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -18,9 +22,44 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class DeliveryFormTest {
 
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    String dateMeeting = generateDate(7);
+
     @BeforeEach
     void setUp() {
+        Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
+    }
+
+    @Test
+    void shouldShowDirectInput(){
+        $("span[data-test-id='city'] input").setValue("Краснодар");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue("22.08.2022");
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("+79999999999");
+        $("[data-test-id=agreement]").click();
+        $(By.className("button")).click();
+        $("[data-test-id=notification]")
+                .$(withText("Встреча успешно забронирована"))
+                .shouldBe(visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    void shouldShowDateCalculation() {
+        $("span[data-test-id='city'] input").setValue("Краснодар");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(generateDate(7));
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("+79999999999");
+        $("[data-test-id=agreement]").click();
+        $(By.className("button")).click();
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + dateMeeting), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 
     @Test
@@ -28,19 +67,18 @@ public class DeliveryFormTest {
         $("span[data-test-id='city'] input").setValue("Кр");
         $$("div.popup__content div").find(exactText("Краснодар")).click();
         $("span[data-test-id='date'] button").click();
-        Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.DAY_OF_MONTH, 15);
-        SimpleDateFormat rusDateFormat = new SimpleDateFormat("dd.MM.yyyy");
-        String dateMeeting = rusDateFormat.format(calendar.getTime());
-        SelenideElement dateElement = $("[data-test-id=date] input[class=input__control]");
-        dateElement.sendKeys("\b\b\b\b\b\b\b\b\b\b");
-        dateElement.setValue(dateMeeting);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+
+
+
         $("[data-test-id=name] input").setValue("Иванов Иван");
         $("[data-test-id=phone] input").setValue("+79999999999");
         $("[data-test-id=agreement]").click();
         $(By.className("button")).click();
-        $("[data-test-id=notification]").$(withText("Встреча успешно забронирована"))
-                .shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + dateMeeting), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 }
+
 
